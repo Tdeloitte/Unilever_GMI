1539f67c0d253fe457182c959145b429967bfe4e
Bisht <sacbisht@deloitte.com>
2021-12-20 17:51:28 +0530

    Commits for width changes

--- src/app/modules/main/layout/header/header.component.ts
+++ src/app/modules/main/layout/header/header.component.ts
@@ -7,17 +7,20 @@ import { CommonService } from 'src/app/services/common.service';
 })
 export class HeaderComponent implements OnInit {
   headerType!: String;
+  colorBlue: boolean=false;
   constructor(private commonService: CommonService) { }
   
   ngOnInit(): void {
     this.commonService.reportType.subscribe((res)=>{this.headerType=res;
       if(this.headerType !=="Home"){
         let element=document.getElementById("navbar");
-        if(element != null){
-          element.style.backgroundColor="#005eef";
-          element.style.color="#fff";
-        
+        this.colorBlue=true;     
+        if(element !== null){
+        element.style.backgroundColor="#005eef";
         }
+      }
+      else{
+     this.colorBlue=false;
       }
     }
     );
