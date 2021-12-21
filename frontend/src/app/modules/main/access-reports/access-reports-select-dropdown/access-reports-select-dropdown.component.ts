bb543f433c2f61ccb0d8f452ed8b8152c982453e
Dodiya <adodiya.ext@deloitte.com>
2021-12-21 15:39:39 +0530

    Styling for filter dropdown

--- frontend/src/app/modules/main/access-reports/access-reports-select-dropdown/access-reports-select-dropdown.component.ts
+++ frontend/src/app/modules/main/access-reports/access-reports-select-dropdown/access-reports-select-dropdown.component.ts
@@ -1,42 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-access-reports-select-dropdown',
   templateUrl: './access-reports-select-dropdown.component.html',
-  styleUrls: ['./access-reports-select-dropdown.component.scss']
+  styleUrls: ['./access-reports-select-dropdown.component.scss'],
 })
 export class AccessReportsSelectDropdownComponent implements OnInit {
+  openDropdown = false;
+  constructor() {}
 
-  constructor() { }
-  
- 
-  ngOnInit(): void {
-    let selected : any = document.querySelector(".selected");
-    let optionsContainer = document.querySelector(".options-container");
-  
-    let optionsList = document.querySelectorAll(".option");
-    if(selected !== null  ){
-    selected.addEventListener("click", () => {
-      if(optionsContainer !== null)
-      optionsContainer.classList.toggle("active");
-    });
+  ngOnInit(): void {}
+
+  onOpenDropdown() {
+    this.openDropdown = !this.openDropdown;
   }
-    
-    optionsList.forEach(o => {
-      
-      o.addEventListener("click", () => {
-        if(selected !== null ){
-          if( o != null){
-            selected.innerHTML = o.querySelector("label")?.innerHTML;
-          }     
-          if(optionsContainer != null)
-          optionsContainer.classList.remove("active");
-        }
-     
-      });
-    });
-  }
-
-
 }
