b9c4b7d7541e7b8029cb6ec049b6c56d0b88d343
apoorvdodiya-deloitte <adodiya.ext@deloitte.com>
2022-01-14 19:04:53 +0530

    Added Metrics table

--- frontend/src/app/modules/main/layout/header/header.component.ts
+++ frontend/src/app/modules/main/layout/header/header.component.ts
@@ -1,55 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CommonService } from 'src/app/services/common.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   headerType!: String;
-  colorBlue: boolean=false;
-  AccessReports: boolean=false;
-  HomeFlag: boolean=false;
-  constructor(private commonService: CommonService,private router:Router) { }
-  
+  colorBlue: boolean = false;
+  AccessReports: boolean = false;
+  HomeFlag: boolean = false;
+  pages = {
+    accessReports: {
+      color: '',
+      icon: 'assets/images/Access-reports-header.png',
+      title: 'Access Reports',
+    },
+  };
+  constructor(
+    private commonService: CommonService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
   ngOnInit(): void {
-    this.commonService.reportType.subscribe((res)=>{this.headerType=res;
+    this.commonService.reportType.subscribe((res) => {
+      this.headerType = res;
       this.checkHeaderType();
-      if(this.headerType !=="Home"){
-        let element=document.getElementById("navbar");
-        this.colorBlue=true;     
-        if(element !== null){
-        element.style.backgroundColor="#005eef";
+      if (this.headerType !== 'Home') {
+        let element = document.getElementById('navbar');
+        this.colorBlue = true;
+        if (element !== null) {
+          element.style.backgroundColor = '#005eef';
+        }
+      } else {
+        this.colorBlue = false;
+        let element = document.getElementById('navbar');
+        if (element !== null) {
+          element.style.backgroundColor = '#fff';
         }
       }
-      else{
-     this.colorBlue=false;
-     let element=document.getElementById("navbar");    
-        if(element !== null){
-        element.style.backgroundColor="#fff";
-        }
-      }
-    }
-    );
-    
+    });
   }
 
-  checkHeaderType(){
-     if(this.headerType == "Home"){
-      this.HomeFlag=true;
+  checkHeaderType() {
+    if (this.headerType == 'Home') {
+      this.HomeFlag = true;
+    } else if (this.headerType == 'Access reports') {
+      this.AccessReports = true;
+      this.HomeFlag = false;
+    } else if (this.headerType == 'Export data') {
+      this.AccessReports = false;
+      this.HomeFlag = false;
     }
-    else if(this.headerType == "Access reports"){
-      this.AccessReports=true;
-      this.HomeFlag=false;
-    }
-    else if(this.headerType == "Export data"){
-      this.AccessReports=false;
-      this.HomeFlag=false;
-    }  
   }
 
-  navigateToHome(){
-    this.router.navigate(["./dashboard"]);
+  navigateToHome() {
+    this.router.navigate(['./dashboard']);
   }
 }
