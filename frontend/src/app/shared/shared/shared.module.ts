a3c5d23c3f319673a9817ed7358afeff4c179aee
apoorvdodiya-deloitte <adodiya.ext@deloitte.com>
2022-01-18 12:14:10 +0530

    added mat tooltip

--- frontend/src/app/shared/shared/shared.module.ts
+++ frontend/src/app/shared/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TooltipComponent } from './tooltip/tooltip.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [TooltipComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MatTooltipModule],
   exports: [TooltipComponent, ReactiveFormsModule],
 })
 export class SharedModule {}
